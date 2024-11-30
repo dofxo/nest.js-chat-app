c57c8ed40262c5babf2391ee393adac928043245
Mohammad Kargar <kargat504@gmail.com>
2024-12-01 02:01:12 +0330

    feat: add POST route for users module
    still needs to be worked on. just added it for database testing

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,13 @@
-import { Controller, Get, Param } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
 import { UsersService } from "./users.service";
 
 @Controller("users")
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  async createUser(@Body("name") name: string) {
+    const user = await this.usersService.createUser(name);
+    return user;
+  }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from "@nestjs/common";
+import { DatabaseService } from "src/database/database.service";
+import mongoose from "mongoose";
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private userModel;
+
+  constructor(private readonly databaseService: DatabaseService) {
+    const User = mongoose.model("Users", new mongoose.Schema({ name: String }));
+
+    this.userModel = User;
+  }
+
+  async createUser(name: string) {
+    const newUser = new this.userModel({ name });
+    newUser.save();
+
+    return newUser;
+  }
+}
