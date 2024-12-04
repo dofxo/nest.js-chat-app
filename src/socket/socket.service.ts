b03bd72e954eb57dbf64a76f2fb89f4454f7b43c
Mohammad Kargar <kargat504@gmail.com>
2024-12-04 21:40:38 +0330

    feat: send date and username with the socket message emit

--- src/socket/socket.service.ts
+++ src/socket/socket.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from "@nestjs/common";
 import { Server } from "socket.io";
+import * as cookie from "cookie";
+import { JwtService } from "@nestjs/jwt";
 
 @Injectable()
 export class SocketService {
   private io: Server;
+
+  constructor(private readonly jwtService: JwtService) {}
 
   initialize(server: any) {
     this.io = new Server(server, {
@@ -19,15 +23,31 @@ export class SocketService {
         console.log(`Client disconnected: ${socket.id} (Reason: ${reason})`);
       });
 
+      const rawCookie = socket.handshake.headers.cookie || "";
+      const parsedCookies = cookie.parse(rawCookie);
+
+      const token = parsedCookies.token;
+
       // listens for new message
-      this.newMessage(socket);
+      this.newMessage(socket, token);
     });
   }
 
-  newMessage(socket: any) {
+  newMessage(socket: any, token: string) {
     socket.on("message", (message: any) => {
+      let name;
+      if (token) {
+        name = this.jwtService.decode(token).name;
+      } else {
+        name = "Anonymous";
+      }
+
       // send incoming message to all clients
-      this.io.emit("message", message);
+      this.io.emit("message", {
+        message,
+        date: new Date(),
+        username: name,
+      });
     });
   }
 }
